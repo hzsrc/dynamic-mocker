7124ccb7de51ffb7437429087faf5a25df02b15b
huangzheng <zheng.huang@flaginfo.com.cn>
2018-12-25 13:19:51 +0800

    up

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 const config = {
   mockEnabled: true,
   mockPath: ['mock/root'], //模拟文件根目录
-  proxyTarget: 'https://bizapi-t1.umss.cn', //后台接口服务地址（代理目标），为空表示不代理
+  proxyTarget: '//www.abc.com', //后台接口服务地址（代理目标），为空表示不代理
   isHttps: false, //是否https
   port: 8037, //端口
   checkPath: function (urlPath) { //urlPath校验函数，返回true表示需要进行mock处理，为false直接走代理
     return true
   },
   beforeResponse: function (respData, req) { //数据返回前的回调钩子，respData包含status、headers、body属性
+    respData.headers['Access-Control-Allow-Origin'] = req.headers['origin'] || req.headers['Origin'] || '';
     if (req.method === 'OPTIONS') {
-      respData.headers['Access-Control-Allow-Origin'] = req.headers['origin'] || req.headers['Origin'] || '';
       respData.headers['Access-Control-Allow-Headers'] = req.headers['access-control-request-headers'] || req.headers['Access-Control-Request-Headers'] || '';
       respData.headers['Access-Control-Allow-Credentials'] = 'true';
       respData.headers['Access-Control-Max-Age'] = '6000';
